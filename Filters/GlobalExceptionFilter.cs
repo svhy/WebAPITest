aeb7fadddba022f540f0bd2eac99bcdc34a9533d
svhy <svhy@qq.com>
2020-12-30 12:43:26 +0800

    test

---
+++ Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Net;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPITest.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+
+        private readonly ILogger<GlobalExceptionFilter> _logger;
+
+        public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            Exception ex = context.Exception;
+            _logger.LogError("出现未知异常:{0}", ex.Message);
+
+            var requestData = context.HttpContext.Request.Headers.ContainsKey("x-requested-with");
+            bool IsAjax = false;
+            if (requestData)
+            {
+                IsAjax = context.HttpContext.Request.Headers["x-requested-with"] == "XMLHttpRequest";
+            }
+
+            //测试
+            if (!IsAjax)
+            {
+                context.Result = new JsonResult(new { Code = 500, Msg = "System Error." });
+            }
+            else
+            {
+                context.Result = new JsonResult(new { Code = 500, Msg = "System Error." });
+            }
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebAPITest.Filters;
+
+namespace WebAPITest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(option =>
+            {
+                option.Filters.Add<GlobalExceptionFilter>();
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Nginx获取IP问题
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
